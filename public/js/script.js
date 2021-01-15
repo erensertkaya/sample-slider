c9cc958a6fd4dec5c0aecd839f813baf132c92c4
Eren <eren.sertkaya.es@gmail.com>
2021-01-15 22:12:51 +0300

    almost done

--- public/js/script.js
+++ public/js/script.js
@@ -10,11 +10,18 @@ document.addEventListener('DOMContentLoaded', function () {
         return result
     }
 
+   const getData = async (start, stop) => {
+        let result = await getSamples();
+        let slicedResult = result.slice(start, stop)
+        return slicedResult
+    }
+
 
 
     const setSamplesFirstWidget = () => {
-        getSamples().then((data) => {
-            const  list =  document.getElementById("splide__list")
+        getData(0,10).then((data) => {
+            const firstWidget = document.querySelector("#first-slider > .splide__track");
+            const  list =  firstWidget.querySelector(".splide__list")
             for (var i=0; i<10; i++){
                 list.innerHTML += "<div class=\"splide__slide\">\n" +
                     "                    <div class=\"item\">\n" +
@@ -35,14 +42,68 @@ document.addEventListener('DOMContentLoaded', function () {
                     "                </div>"
             }
 
-          /*  if (data[i].params.land !== ''){
+        })
+    }
 
-                /!*<i class="icon-new"></i>*!/
-            }*/
+    const setSampleSecondWidget = () => {
+        getData(10,20).then((data) => {
+            console.log(data);
+            const secondWidget = document.querySelector("#secondary-slider > .splide__track");
+            const  list =  secondWidget.querySelector(".splide__list")
+            for (var i=0; i<10; i++){
+                list.innerHTML += "<div class=\"splide__slide\">\n" +
+                    "                    <div class=\"item\">\n" +
+                    "                        <div class=\"item-image-container\">\n" +
+                    "                            <img src="+data[i].image+" alt=\"\">\n" +
+                    "                            <i class=\"icon-discount\"></i>\n" +
+                    "                            <i class=\"icon-new\">"+data[i].params.land.substring(1, 4)+"</i>\n" +
+                    "                            <i class=\"icon-fav\">"+data[i].params.likeCount+"</i>\n" +
+                    "                        </div>\n" +
+                    "                        <span class=\"item-title\">"+data[i].name+"</span>\n" +
+                    "                        <span class=\"item-categories\">"+data[i].params.rebsorte+"</span>\n" +
+                    "                        <div class=\"item-text-container\">\n" +
+                    "                        <span class=\"item-price\">"+data[i].priceText+"</span>\n" +
+                    "                        <span class=\"item-oldprice\">"+data[i].oldPriceText+"</span>\n" +
+                    "                        <span class=\"item-detail\">"+data[i].params.basePrice+"</span>\n" +
+                    "                        </div>\n" +
+                    "                    </div>\n" +
+                    "                </div>"
+            }
+
+        })
+    }
+
+    const setSampleThirdWidget = () => {
+        getData(20,30).then((data) => {
+            console.log(data);
+            const thirdWidget = document.querySelector("#third-slider > .splide__track");
+            const  list =  thirdWidget.querySelector(".splide__list")
+            for (var i=0; i<10; i++){
+                list.innerHTML += "<div class=\"splide__slide\">\n" +
+                    "                    <div class=\"item\">\n" +
+                    "                        <div class=\"item-image-container\">\n" +
+                    "                            <img src="+data[i].image+" alt=\"\">\n" +
+                    "                            <i class=\"icon-discount\"></i>\n" +
+                    "                            <i class=\"icon-new\">"+data[i].params.land.substring(1, 4)+"</i>\n" +
+                    "                            <i class=\"icon-fav\">"+data[i].params.likeCount+"</i>\n" +
+                    "                        </div>\n" +
+                    "                        <span class=\"item-title\">"+data[i].name+"</span>\n" +
+                    "                        <span class=\"item-categories\">"+data[i].params.rebsorte+"</span>\n" +
+                    "                        <div class=\"item-text-container\">\n" +
+                    "                        <span class=\"item-price\">"+data[i].priceText+"</span>\n" +
+                    "                        <span class=\"item-oldprice\">"+data[i].oldPriceText+"</span>\n" +
+                    "                        <span class=\"item-detail\">"+data[i].params.basePrice+"</span>\n" +
+                    "                        </div>\n" +
+                    "                    </div>\n" +
+                    "                </div>"
+            }
+
         })
     }
 
     setSamplesFirstWidget()
+    setSampleSecondWidget()
+    setSampleThirdWidget()
     var options = {
         // Define them separately in the ttb(vertical) mode.
         type: 'loop',
@@ -51,6 +112,8 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     setTimeout(function(){ new Splide('#first-slider', options).mount(); }, 100);
+    setTimeout(function(){ new Splide('#secondary-slider', options).mount(); }, 100);
+    setTimeout(function(){ new Splide('#third-slider', options).mount(); }, 100);
 
 });
 
