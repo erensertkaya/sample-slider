6a37741b2278b4a3109bbf730e852ce869afb661
Eren <eren.sertkaya.es@gmail.com>
2021-01-15 23:23:11 +0300

    .

--- public/js/script.js
+++ public/js/script.js
@@ -27,9 +27,9 @@ document.addEventListener('DOMContentLoaded', function () {
                     "                    <div class=\"item\">\n" +
                     "                        <div class=\"item-image-container\">\n" +
                     "                            <img src="+data[i].image+" alt=\"\">\n" +
-                    "                            <i class=\"icon-discount\"></i>\n" +
-                    "                            <i class=\"icon-new\">"+data[i].params.land.substring(1, 4)+"</i>\n" +
-                    "                            <i class=\"icon-fav\">"+data[i].params.likeCount+"</i>\n" +
+                    "                            <i class=\"icon-discount\"><p></p></i>\n" +
+                    "                            <i class=\"icon-new\"><p>"+data[i].params.land.substring(1, 4)+"</p></i>\n" +
+                    "                            <i class=\"icon-fav\"><p>"+data[i].params.likeCount+"</p></i>\n" +
                     "                        </div>\n" +
                     "                        <span class=\"item-title\">"+data[i].name+"</span>\n" +
                     "                        <span class=\"item-categories\">"+data[i].params.rebsorte+"</span>\n" +
@@ -41,13 +41,14 @@ document.addEventListener('DOMContentLoaded', function () {
                     "                    </div>\n" +
                     "                </div>"
             }
-
+            if (list.querySelector(".icon-new p").innerText === ''){
+                list.querySelector(".icon-new").innerHTML = ''
+            }
         })
     }
 
     const setSampleSecondWidget = () => {
         getData(10,20).then((data) => {
-            console.log(data);
             const secondWidget = document.querySelector("#secondary-slider > .splide__track");
             const  list =  secondWidget.querySelector(".splide__list")
             for (var i=0; i<10; i++){
@@ -55,9 +56,9 @@ document.addEventListener('DOMContentLoaded', function () {
                     "                    <div class=\"item\">\n" +
                     "                        <div class=\"item-image-container\">\n" +
                     "                            <img src="+data[i].image+" alt=\"\">\n" +
-                    "                            <i class=\"icon-discount\"></i>\n" +
-                    "                            <i class=\"icon-new\">"+data[i].params.land.substring(1, 4)+"</i>\n" +
-                    "                            <i class=\"icon-fav\">"+data[i].params.likeCount+"</i>\n" +
+                    "                            <i class=\"icon-discount\"><p></p></i>\n" +
+                    "                            <i class=\"icon-new\"><p>"+data[i].params.land.substring(1, 4)+"</p></i>\n" +
+                    "                            <i class=\"icon-fav\"><p>"+data[i].params.likeCount+"</p></i>\n" +
                     "                        </div>\n" +
                     "                        <span class=\"item-title\">"+data[i].name+"</span>\n" +
                     "                        <span class=\"item-categories\">"+data[i].params.rebsorte+"</span>\n" +
@@ -75,17 +76,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const setSampleThirdWidget = () => {
         getData(20,30).then((data) => {
-            console.log(data);
             const thirdWidget = document.querySelector("#third-slider > .splide__track");
             const  list =  thirdWidget.querySelector(".splide__list")
             for (var i=0; i<10; i++){
-                list.innerHTML += "<div class=\"splide__slide\">\n" +
+                list.innerHTML += <div class=\"splide__slide\">\n" +
                     "                    <div class=\"item\">\n" +
                     "                        <div class=\"item-image-container\">\n" +
                     "                            <img src="+data[i].image+" alt=\"\">\n" +
-                    "                            <i class=\"icon-discount\"></i>\n" +
-                    "                            <i class=\"icon-new\">"+data[i].params.land.substring(1, 4)+"</i>\n" +
-                    "                            <i class=\"icon-fav\">"+data[i].params.likeCount+"</i>\n" +
+                    "                            <i class=\"icon-discount\"><p></p></i>\n" +
+                    "                            <i class=\"icon-new\"><p>"+data[i].params.land.substring(1, 4)+"</p></i>\n" +
+                    "                            <i class=\"icon-fav\"><p>"+data[i].params.likeCount+"</p></i>\n" +
                     "                        </div>\n" +
                     "                        <span class=\"item-title\">"+data[i].name+"</span>\n" +
                     "                        <span class=\"item-categories\">"+data[i].params.rebsorte+"</span>\n" +
@@ -101,9 +101,20 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 
+    const checkUndefineds = () => {
+      const list = document.querySelectorAll("#splide__list")
+        list.forEach((each) => {
+       console.log(each.getElementsByClassName("icon-fav")[1])
+        })
+
+    }
+
+
     setSamplesFirstWidget()
     setSampleSecondWidget()
     setSampleThirdWidget()
+    checkUndefineds()
+
     var options = {
         // Define them separately in the ttb(vertical) mode.
         type: 'loop',
